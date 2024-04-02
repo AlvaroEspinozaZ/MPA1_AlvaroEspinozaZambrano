48fd168bff29d0f18b3ff8440d0e41ffd48015d5
AlvaroEspinozaZ <130480207+AlvaroEspinozaZ@users.noreply.github.com>
2024-04-02 17:29:25 -0500

    curvaCasi

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -15,6 +15,7 @@ public class GameController : MonoBehaviour
     float yMax = 0;
     public float timer = 0;
     bool iniciarTImer=false;
+    public float curva = 0;
     void Start()
     {
         startPosition = selectedObject.transform.position;
@@ -25,8 +26,19 @@ public class GameController : MonoBehaviour
     {
         if (iniciarTImer)
         {
+           
             timer += Time.deltaTime;
+            if (timer > 1.3f)
+            {
+                curva = Mathf.Clamp(curva + Time.deltaTime, -1, 1);
+                if (curva == 1)
+                {
+                    curva = 0;
+                    curva = Mathf.Clamp(curva - Time.deltaTime, -1, 1);
+                }
+            }
         }
+
     }
     private void FixedUpdate()
     {
@@ -44,7 +56,8 @@ public class GameController : MonoBehaviour
                     if (hit.collider.tag == "Player")
                     {
                         selectedObject.transform.position = new Vector3(hit.point.x, hit.point.y, selectedObject.transform.position.z);
-                        Debug.Log(selectedObject.transform.position.y);
+                        Debug.Log("Y "+selectedObject.transform.position.y);
+                        Debug.Log("X " +selectedObject.transform.position.x);
                     }
                    
                 }
@@ -55,12 +68,19 @@ public class GameController : MonoBehaviour
                
                 Debug.Log(yMax);
                 float comprobar = yMax - selectedObject.transform.position.y;
-                Debug.Log("asdad" + comprobar);
+                //Debug.Log("asdad" + comprobar);
                 if (timer < 1.3f && selectedObject.transform.position.y >1.6f)
                 {
                     Debug.Log("LanzamientoRapido");
                     selectedObject.GetComponent<Rigidbody>().useGravity = true;
                     rgb.velocity =new Vector3(0, 1.5f* velocity, 1.5f * velocity);
+                    count--;
+                }
+                if (timer>1.3f && selectedObject.transform.position.y <1.7f && selectedObject.transform.position.x>-0.01f && selectedObject.transform.position.x< 0.006f)
+                {
+                    Debug.Log("LanzamientoCurvao");
+                    selectedObject.GetComponent<Rigidbody>().useGravity = true;                     
+                    rgb.velocity = new Vector3(curva*velocity,0, 1.5f * velocity);
                     count--;
                 }
                 else {
